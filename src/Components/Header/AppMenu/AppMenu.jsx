cf3cba653ce6de186ea04b2d312c6e6df1a068da
monaemhossain <monaemhs@gmail.com>
2023-11-06 23:28:10 +0300

    fixed disaligned hamburger for mobile menu

--- src/Components/Header/AppMenu/AppMenu.jsx
+++ src/Components/Header/AppMenu/AppMenu.jsx
@@ -67,7 +67,7 @@ const ProfileDropDown = (props) => {
 
 const AppMenu = () => {
   const [menuState, setMenuState] = useState(false)
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   const navigation = [
     { title: "Home", path: "/" },
@@ -99,53 +99,55 @@ const AppMenu = () => {
                   ))
                 }
                 {
-                  user?privateNavigation.map((item, idx) => (
+                  user ? privateNavigation.map((item, idx) => (
                     <NavLink to={item.path} key={idx} className={({ isActive, isPending }) =>
                       isPending ? "pending" : isActive ? "text-theme-color-1 cursor-default py-2 px-4 rounded-md border border-theme-color-1" : "border border-transparent hover:border-theme-color-2 py-2 px-4 text-theme-color-1 hover:text-white hover:bg-theme-color-2 transition-all rounded-md"
                     }
                     >
                       {item.title}
                     </NavLink>
-                  )):''
+                  )) : ''
                 }
               </ul>
               <ProfileDropDown
                 class="mt-5 pt-5 border-t lg:hidden"
               />
             </div>
-            <div className="flex-none flex items-center justify-end space-x-2 sm:space-x-6">
+            <div className="lg:flex-none flex-1 flex items-center justify-end space-x-2 sm:space-x-6">
 
 
               <div>
                 <NavLink to="/post-job" className={({ isActive, isPending }) =>
                   isPending ? "pending" : isActive ? "cursor-default bg-theme-color-2 py-3 px-4 rounded-md border border-theme-color-1 text-white" : "border border-theme-color-1 py-2.5 px-4 text-theme-color-1 hover:text-white hover:bg-theme-color-2 transition-all rounded-md"}> Post a Job</NavLink>
+              </div>
+
+
+
+              <div>
+                <ProfileDropDown
+                  class="hidden lg:block"
+                />
+                <button
+                  className="outline-none text-gray-400 block lg:hidden"
+                  onClick={() => setMenuState(!menuState)}
+                >
+                  {
+                    menuState ? (
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                      </svg>
+                    ) : (
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                      </svg>
+                    )
+                  }
+                </button>
+              </div>
+
             </div>
-
-
-
-            <ProfileDropDown
-              class="hidden lg:block"
-            />
-            <button
-              className="outline-none text-gray-400 block lg:hidden"
-              onClick={() => setMenuState(!menuState)}
-            >
-              {
-                menuState ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-                  </svg>
-                )
-              }
-            </button>
-
           </div>
         </div>
-    </div>
       </nav >
     </div >
   )
